435b08dd5ef90de388555e90215539774a1213c2
Brad McCollum <btmcco8@gmail.com>
2018-08-25 16:47:35 +0000

    automatically backed up by learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+    @wheel_size = wheel_size
+    @sheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrroooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
